f7cb294bdeffcb9d31727179a902c78f51dc2f6b
Ciro <ciromoraismedeiros@gmail.com>
2020-03-18 18:34:56 +0100

    Bugfix for variables with null values

--- sparql-server.py
+++ sparql-server.py
@@ -21,7 +21,7 @@ def home():
     query_text = 'SELECT ?s ?p ?o\nWHERE {\n    ?s ?p ?o\n}\nLIMIT 100'
     ans = []    # answers for SELECT queries
     tps = []    # triples for CONSTRUCT queries
-    vrbs = None # query variables
+    vrbs = None # query's selected variables
     
     if request.method == 'POST':
         g = rdflib.Graph()
@@ -31,17 +31,25 @@ def home():
         g.parse(f, format=rdflib.util.guess_format(f))
         tupl = None
         q = g.query(query_text)
+        
         if q.type == 'SELECT':
-            for tupl in q:
-                a = []
-                for k in tupl.asdict():
-                    a += [shorten_iri(tupl[k],g.namespaces())]
-                ans += [a]
             vrbs = q.vars
+            for row in q:
+                row = row.asdict()
+                a = []
+                for k in vrbs:
+                    k = str(k)
+                    v = shorten_iri(row[k],g.namespaces()) if k in row else 'null'
+                    a += [v]
+                ans += [a]
+            
+        
         elif q.type == 'CONSTRUCT':
+            #q.save('data/q.ttl', format='turtle')
             for t in q:
                 t = tuple([shorten_iri(x, g.namespaces()) for x in t])
                 tps += [t]
+
     return render_template('home.html', answers=ans, last=last,
         filenames=filenames, query_text=query_text, 
         variables=vrbs, triples=tps)
